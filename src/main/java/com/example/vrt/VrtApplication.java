8430839693182af5b8aa3da2e4d1209aff5c2b5f
한승우 <peter001019@naver.com>
2025-04-05 22:59:30 +0900

    first commit

---
+++ src/main/java/com/example/vrt/VrtApplication.java
@@ -0,0 +1,13 @@
+package com.example.vrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vrt/VrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vrt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
