75dc3eac182a6fe31d5449284c87fd8c5efd71b3
yslmoment <yslee0005@dgu.ac.kr>
2025-04-08 12:26:58 +0900

    feat : roomController, roomDTO 작성

---
+++ src/main/java/com/example/vrt/domain/room/dto/roomJoinResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.vrt.domain.room.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RoomJoinResponseDTO {
+    private int roomId;
+    private String galleryId;
+    private String hostUserEndpoint;
+    private boolean isHost;
+    private String mapFileURL;
+}
