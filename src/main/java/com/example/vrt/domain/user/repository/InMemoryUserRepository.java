fe6f0e1db688ce5a75c32c40d5704c498f115941
yslmoment <yslee0005@dgu.ac.kr>
2025-05-06 14:50:27 +0900

    refactor : User 내부코드 리팩토링

--- src/main/java/com/example/vrt/domain/user/repository/InMemoryUserRepository.java
+++ src/main/java/com/example/vrt/domain/user/repository/InMemoryUserRepository.java
@@ -1,3 +1,4 @@
+// InMemoryUserRepository.java
 package com.example.vrt.domain.user.repository;
 
 import com.example.vrt.domain.user.entity.User;
@@ -5,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
@@ -12,12 +14,18 @@ public class InMemoryUserRepository implements UserRepository {
     private final Map<String, User> store = new HashMap<>();
 
     @Override
-    public void save(User user) {
+    public User save(User user) {
         store.put(user.getId(), user);
+        return user;
     }
 
     @Override
-    public User findById(String id) {
-        return store.get(id);
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public boolean existsById(String id) {
+        return store.containsKey(id);
     }
 }
