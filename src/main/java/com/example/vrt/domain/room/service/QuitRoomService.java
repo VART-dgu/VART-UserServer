538156a1bbe38c19dc2a46d310d4c0607c483bdf
한승우 <peter001019@naver.com>
2025-06-03 19:03:09 +0900

    feat: Host user Endpoint Broadcast 로직 추가

--- src/main/java/com/example/vrt/domain/room/service/QuitRoomService.java
+++ src/main/java/com/example/vrt/domain/room/service/QuitRoomService.java
@@ -5,12 +5,14 @@ import com.example.vrt.domain.room.dto.RoomQuitResponseDTO;
 import com.example.vrt.domain.room.entity.Room;
 import com.example.vrt.domain.room.repository.RoomRepository;
 import com.example.vrt.global.ping.WebSocketPingService;
+import com.example.vrt.global.websocket.UserService.UserEndpointService;
+import com.example.vrt.global.websocket.UserService.UserMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.WebSocketSession;
 
-import java.net.InetSocketAddress;
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -23,9 +25,8 @@ import java.util.concurrent.ConcurrentHashMap;
 public class QuitRoomService {
     private final RoomRepository roomRepository;
     private final WebSocketPingService webSocketPingService;
-    private final ConcurrentHashMap<String, WebSocketSession> sessionMap;
-    private final ConcurrentHashMap<String, String> sessionIdToUserIdMap;
-    private final ConcurrentHashMap<String, Set<String>> userIdToSessionsMap;
+    private final UserEndpointService userEndpointService;
+    private final UserMessageService userMessageService;
 
     public RoomQuitResponseDTO quitRoom(RoomQuitRequestDTO requestDTO) {
         //1. 사용자 제거
@@ -45,40 +46,36 @@ public class QuitRoomService {
         }
 
         //2. 호스트라면 모든 사용자에게 핑 테스트, 가장 낮은 핑을 새로운 호스트로 선정, 저장
-        if(requestDTO.getIsHost()){
-            //각 participant에 ping 채우기
+        if(requestDTO.getIsHost()) {
+            //ping 테스트로 host ID 찾기
             Optional<String> newHostId = webSocketPingService.findFastestUser(room.getParticipantIDs());
 
             newHostId.ifPresentOrElse(hostId -> {
-                Set<String> sessionIds = userIdToSessionsMap.get(hostId);
+                //hostId 설정
+                room.setHostUserId(hostId);
+                //host ID 기반으로 host endpoint 검색
+                Optional<String> hostEndpoint = userEndpointService.findAnyEndpointByUserId(hostId);
 
-                if (sessionIds == null || sessionIds.isEmpty()) {
-                    log.warn("호스트로 선정되었으나 열린 WebSocket 세션이 없습니다. hostId={}", hostId);
-                    return;
-                }
+                hostEndpoint.ifPresentOrElse(endpoint -> {
+                    //host Endpoint 설정
+                    room.setHostUserEndpoint(endpoint);
 
-                for (String sessionId : sessionIds) {
-                    WebSocketSession session = sessionMap.get(sessionId);
-
-                    if (session == null || !session.isOpen()) {
-                        continue;
+                    //방의 모든 사용자에게 host endpoint 전송
+                    for (String userId : room.getParticipantIDs()) {
+                        try {
+                            userMessageService.sendTextToUser(userId, endpoint);
+                        } catch (IOException e) {
+                            log.error("호스트 ID={} 세션으로 메시지 전송 중 오류 발생", hostId, e);
+                        }
                     }
+                }, () -> {
+                    log.warn("주어진 hostId={} 로 열린 WebSocket 세션을 찾지 못했습니다.", hostId);
+                });
+            }, () -> {
+                log.warn("Host를 발견하지 못했습니다.");
+            });
 
-                    InetSocketAddress remote = session.getRemoteAddress();
-
-                    if (remote != null) {
-                        String endpoint = remote.getAddress().getHostAddress() + ":" + remote.getPort();
-                        log.info("호스트의(사용자 {})의 엔드포인트: {}", hostId, endpoint);
-                    } else {
-                        log.info("호스트(사용자 {})의 WebSocketSession은 있지만 엔드포인트를 얻지 못했습니다.", hostId);
-                    }
-                }
-            },
-                    () -> {
-                log.warn("호스트를 설정하지 못했습니다.");
-                    });
-
-        //3. 새로운 호스트 broadcast
+        }
 
         return RoomQuitResponseDTO.builder()
                 .roomId(room.getId())
